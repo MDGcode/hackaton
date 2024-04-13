f2795ef93e78e93d448f6fe86be9d665df524ef3
MDGcode <adrianboscanici@yahoo.com>
2024-04-14 01:18:15 +0300

    ceva

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,25 @@
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 import TweetText from "../components/TweetText";
+import { Tweet } from "react-tweet";
+import { Timeline } from "react-twitter-widgets";
 
 export default function Home() {
   return (
     <>
       <TweetText />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        userId={"1779182497764372480"}
+        options={{ height: 400 }}
+      />
+      <Tweet id="1779217258293731722" />
+      <Timeline
+        dataSource={{
+          sourceType: "profile",
+          screenName: "starplus",
+        }}
+        options={{ height: 400 }}
+      />
     </>
   );
 }
