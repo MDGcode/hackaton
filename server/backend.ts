c9c9dc4eb9a5b35f1a452dc4ab5a7761fcd8ab1d
Mihnea <asmihnea5@gmail.com>
2024-04-13 15:38:13 +0300

    Upload Photo Function

--- server/backend.ts
+++ server/backend.ts
@@ -1,6 +1,8 @@
 import { GenezioDeploy } from "@genezio/types";
 import { IgApiClient } from "instagram-private-api";
-
+import { promisify } from "util";
+import { readFile } from 'fs';
+const readFileAsync = promisify(readFile);
 
 @GenezioDeploy()
 export class BackendService {
@@ -16,8 +18,8 @@ export class BackendService {
     await this.ig.account.login("alexandru.georgescu22@gmail.com", "@lex@ndru22");
   }
 
-  async test() {
-
+  async UploadPhoto(captionString: string) {
+    try{
       this.ig.state.generateDevice("alexandru.georgescu22@gmail.com");
       
       await this.ig.simulate.preLoginFlow();
@@ -26,12 +28,65 @@ export class BackendService {
       process.nextTick(async () => await this.ig.simulate.postLoginFlow());
 
       const userFeed = this.ig.feed.user(loggedInUser.pk);
-      const myPostsFirstPage = await userFeed.items();
+      // const myPostsFirstPage = await userFeed.items();
 
-      const myPostsSecondPage = await userFeed.items();
+      // const myPostsSecondPage = await userFeed.items();
       
-      return ;
+      const path = "../client/src/assets/test.jpg";
+
+      const { latitude, longitude, searchQuery } = {
+        latitude: 0.0,
+        longitude: 0.0,
+      };
+
+    const locations = await this.ig.search.location(latitude, longitude, searchQuery);
+
+    const mediaLocation = locations[0];
+    console.log(mediaLocation);
+  
+  const publishResult = await this.ig.publish.photo({
+      // read the file into a Buffer
+      file: await readFileAsync(path),
+      // optional, default ''
+      caption: captionString,
+      // optional
+      location: mediaLocation,
+    });
+
+  return readFileAsync(path);
+
+  }catch(error){
+    return error;
+  } 
   }
+
+
+  // async uploadPhoto() {
+    
+
+  //     const path = "../client/src/assets/test.jpg";
+
+  //     const { latitude, longitude, searchQuery } = {
+  //       latitude: 0.0,
+  //       longitude: 0.0,
+  //     };
+
+  //     const locations = await this.ig.search.location(latitude, longitude, searchQuery);
+
+  //   const mediaLocation = locations[0];
+  //   console.log(mediaLocation);
+  
+  //   const publishResult = await this.ig.publish.photo({
+  //     // read the file into a Buffer
+  //     file: await readFileAsync(path),
+  //     // optional, default ''
+  //     caption: 'my caption',
+  //     // optional
+  //     location: mediaLocation,
+  //   });
+
+  //   console.log(publishResult);
+  // }
 
 //   async test() {
 //     const ACCESS_TOKEN = "385492911016970"
